df7f81200e3b6b6f40862d96e308f7a7b449ec30
Cris <c3cris@gmail.com>
2016-10-31 00:41:21 -0700

    Add title

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ define('mypage',true);
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
+	<title>Double Infinity</title>
 	<link rel="stylesheet" href="main.css">
 </head>
 <body>
